7e740e4a28642e31851335b54e3560b770206225
Preetham gowda n <prtmpreethu@gmail.com>
2026-01-21 22:05:11 +0530

    Create function1.js
    function to add, subtract, multiply and divide a two number

---
+++ 21-01/Function to add,subtract,multiply & divide 2 numbers/function1.js
@@ -0,0 +1,31 @@
+/*function to add 2 numbers*/
+function add(){
+  let a= 10;
+  let b= 20;
+  let sum=a+b;
+  console.log(`the sum of a and b is ${sum}`);
+}
+
+/*function to subtract a number from a number number*/
+function subtract(){
+  let a= 10;
+  let b= 20;
+  let c=a-b;
+  console.log(`the value of a - b is ${c}`);
+}
+
+/*function to multiply 2 numbers*/
+function multiply(){
+  let a= 10;
+  let b= 20;
+  let product=a*b;
+  console.log(`the product of a and b is ${product}`);
+}
+
+/*function to add 2 numbers*/
+function divide(){
+  let a= 10;
+  let b= 20;
+  let c=a/b;
+  console.log(`the value of a / b is ${c}`);
+}
