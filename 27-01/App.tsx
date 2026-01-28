ff5789b7dc560278bcb5b40bf6faab3505c476d0
Preetham gowda n <prtmpreethu@gmail.com>
2026-01-28 10:19:07 +0530

    Update App.tsx

--- 27-01/App.tsx
+++ 27-01/App.tsx
@@ -1,1 +1,29 @@
+import { useState } from "react";
 
+export default function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const incrementByTwo = () => {
+    setCount((prev) => prev + 2);
+  };
+
+  const decrementByOne = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+  
+
+      <h2>Count: {count}</h2>
+
+      <button onClick={incrementByTwo} style={{ marginRight: "10px" }}>
+        Increment 
+      </button>
+
+      <button onClick={decrementByOne}>
+        Decrement 
+      </button>
+    </div>
+  );
+}
